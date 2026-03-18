11fefe0a5d27355c3aa4206e324cfe7340e0a3ea
Ushna Faheem <ushnafaheem616@gmail.com>
2026-03-19 00:29:14 +0500

    navigation fixes

--- app/src/main/java/com/example/newsapp/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/example/newsapp/ui/screens/SplashScreen.kt
@@ -41,8 +41,8 @@ fun SplashScreen(navController: NavController) {
 
         delay(2500)
 
-        navController.navigate(Routes.Home.route) {
-            popUpTo(0)
+        navController.navigate("main_tabs") {
+            popUpTo(Routes.Splash.route) { inclusive = true } // remove Splash from backstack
             launchSingleTop = true
         }
     }
@@ -78,63 +78,3 @@ fun SplashScreen(navController: NavController) {
         }
     }
 }
-
-//package com.example.newsapp.ui.screens
-//
-//import androidx.compose.foundation.Image
-//import androidx.compose.foundation.background
-//import androidx.compose.foundation.layout.*
-//import androidx.compose.material3.MaterialTheme
-//import androidx.compose.material3.Text
-//import androidx.compose.runtime.*
-//import androidx.compose.ui.Alignment
-//import androidx.compose.ui.Modifier
-//import androidx.compose.ui.res.painterResource
-//import androidx.compose.ui.text.font.FontWeight
-//import androidx.compose.ui.unit.dp
-//import androidx.compose.ui.unit.sp
-//import androidx.navigation.NavController
-//import com.example.newsapp.R
-//import com.example.newsapp.navigation.Routes
-//import kotlinx.coroutines.delay
-//
-//@Composable
-//fun SplashScreen(navController: NavController) {
-//
-//    LaunchedEffect(Unit) {
-//        delay(2500)
-//
-//        navController.navigate(Routes.Home.route) {
-//            popUpTo(0)
-//                launchSingleTop = true
-//        }
-//    }
-//
-//    Box(
-//        modifier = Modifier
-//            .fillMaxSize()
-//            .background(MaterialTheme.colorScheme.background),
-//        contentAlignment = Alignment.Center
-//    ) {
-//
-//        Column(
-//            horizontalAlignment = Alignment.CenterHorizontally
-//        ) {
-//
-//            Image(
-//                painter = painterResource(R.drawable.news_icon),
-//                contentDescription = "News Icon",
-//                modifier = Modifier.size(width = 135.dp, height = 125.dp)
-//            )
-//
-//            Spacer(modifier = Modifier.height(15.dp))
-//
-//            Text(
-//                text = "NewsMate",
-//                fontSize = 32.sp,
-//                fontWeight = FontWeight.Bold,
-//                color = MaterialTheme.colorScheme.secondary
-//            )
-//        }
-//    }
-//}
