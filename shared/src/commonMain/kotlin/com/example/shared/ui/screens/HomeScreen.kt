fc8eff0b061731ee7afd912a6014959040ddba7c
Ushna Faheem <ushnafaheem616@gmail.com>
2026-04-14 14:45:38 +0500

    Implement username fetch in AuthViewModel for home screen

--- shared/src/commonMain/kotlin/com/example/shared/ui/screens/HomeScreen.kt
+++ shared/src/commonMain/kotlin/com/example/shared/ui/screens/HomeScreen.kt
@@ -1,15 +1,23 @@
 package com.example.shared.ui.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.WavingHand
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
@@ -32,23 +40,29 @@ import app.cash.paging.compose.LazyPagingItems
 import app.cash.paging.compose.collectAsLazyPagingItems
 import com.example.shared.NewsAppTheme
 import com.example.shared.data.mock.articleMock
+import com.example.shared.data.models.Article
+import com.example.shared.data.models.NetworkError
 import com.example.shared.ui.components.CategoryChips
 import com.example.shared.ui.components.EmptyState
 import com.example.shared.ui.components.HomeArticleItem
-import com.example.shared.data.models.Article
-import com.example.shared.data.models.NetworkError
 import com.example.shared.utils.Category
 import com.example.shared.utils.DateFormatter
 import com.example.shared.utils.mapErrorToMessage
+import com.example.shared.viewmodels.AuthViewModel
 import com.example.shared.viewmodels.NewsViewModel
+import dev.gitlive.firebase.Firebase
+import dev.gitlive.firebase.auth.auth
 import me.sample.library.resources.Res
-import me.sample.library.resources.*
+import me.sample.library.resources.hello_user
+import me.sample.library.resources.home_desc
 import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun HomeScreen(
-    viewModel: NewsViewModel = koinViewModel(), onArticleClick: (Article) -> Unit
+    viewModel: NewsViewModel = koinViewModel(),
+    authViewModel: AuthViewModel = koinViewModel(),
+    onArticleClick: (Article) -> Unit
 ) {
     val articles = viewModel.homeNewsPagingData.collectAsLazyPagingItems()
 
@@ -59,6 +73,13 @@ fun HomeScreen(
 
     val uiMessage by viewModel.uiMessage.collectAsState(null)
 
+//    var username by remember { mutableStateOf<String?>(null) }
+    val username by authViewModel.username.collectAsState()
+
+    LaunchedEffect(Unit) {
+        authViewModel.loadUsername()
+    }
+
     uiMessage?.let { message ->
 
         val messageText = stringResource(message)
@@ -68,15 +89,26 @@ fun HomeScreen(
         }
     }
 
-    HomeScreenContent(
-        articles = articles,
-        bookmarkedUrls = bookmarkedUrls,
-        onArticleClick = onArticleClick,
-        onBookmarkClick = { viewModel.toggleBookmark(it) },
-        onCategorySelected = { viewModel.setHomeCategory(it) },
-        snackbarHostState = snackbarHostState
+    LaunchedEffect(Unit) {
+        val userId = Firebase.auth.currentUser?.uid
+        if (userId != null) {
+            viewModel.setCurrentUser(userId)
+        }
+    }
 
-    )
+    Scaffold(
+        snackbarHost = { SnackbarHost(snackbarHostState) }
+    ) {
+        padding ->
+        HomeScreenContent(
+            articles = articles,
+            bookmarkedUrls = bookmarkedUrls,
+            onArticleClick = onArticleClick,
+            onBookmarkClick = { viewModel.toggleBookmark(it) },
+            onCategorySelected = { viewModel.setHomeCategory(it) },
+            username = username
+        )
+    }
 }
 
 //stateless
@@ -87,7 +119,7 @@ fun HomeScreenContent(
     onArticleClick: (Article) -> Unit,
     onBookmarkClick: (Article) -> Unit,
     onCategorySelected: (String) -> Unit,
-    snackbarHostState: SnackbarHostState
+    username: String?
 ) {
 
     val dateFormatter = remember { DateFormatter() }
@@ -96,20 +128,31 @@ fun HomeScreenContent(
 
     val listState = rememberLazyListState()
 
-    Scaffold(
-        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
-    ) { padding ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(horizontal = 16.dp)
-        ) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp)
+            .background(MaterialTheme.colorScheme.background)
+    ) {
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
 
             Text(
-                text = (stringResource(Res.string.hello_user)),
+                text = username?.let { "Hello, $it" } ?: "",
                 style = MaterialTheme.typography.headlineLarge,
                 modifier = Modifier.padding(top = 16.dp)
             )
+
+            Spacer(modifier = Modifier.width(6.dp))
+            Icon(
+                imageVector = Icons.Default.WavingHand,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.primary,
+                modifier = Modifier.size(42.dp)
+                    .padding(top = 12.dp)
+            )
+        }
 
             Text(
                 text = (stringResource(Res.string.home_desc)),
@@ -237,7 +280,6 @@ fun HomeScreenContent(
             }
         }
     }
-}
 
 @Composable
 fun HomeScreenPreviewContent(
@@ -251,11 +293,22 @@ fun HomeScreenPreviewContent(
             .padding(horizontal = 16.dp)
     ) {
 
+        Row {
         Text(
             text = stringResource(Res.string.hello_user),
             style = MaterialTheme.typography.headlineLarge,
             modifier = Modifier.padding(top = 16.dp)
         )
+        Spacer(modifier = Modifier.width(6.dp))
+        Icon(
+            imageVector = Icons.Default.WavingHand,
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.size(42.dp)
+                .padding(top = 12.dp)
+        )
+
+    }
 
         Text(
             text = stringResource(Res.string.home_desc),
